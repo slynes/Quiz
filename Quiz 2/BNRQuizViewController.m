6eec5ddbed3398ed6ec723d5ca04466311b1332e
Shane Lynes <shanelynes@host-studentw-139-19.dhcp.stevens-tech.edu>
2015-01-30 16:50:17 -0500

    In the BNRQuizViewController.m i added the properites for the question and answer label, and set the action for the buttons. In the .xib file, I made the connections and adjusted the size of the buttons.

--- Quiz 2/BNRQuizViewController.m
+++ Quiz 2/BNRQuizViewController.m
@@ -10,28 +10,21 @@
 
 @interface BNRQuizViewController ()
 
+@property (nonatomic, weak) IBOutlet UILabel *questionLabel;
+@property (nonatomic, weak) IBOutlet UILabel *answerLabel;
+
 @end
 
 @implementation BNRQuizViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+- (IBAction)showQuestion:(id)sender
+{
+    
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)showAnswer:(id)sender
+{
+    
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
